db388a6e5ed37bcae0f9e4bd3c5edb8467a2b207
nalandagaikwad78 <130948909+nalandagaikwad78@users.noreply.github.com>
2023-10-11 15:43:01 +0530

    commit
    ok

---
+++ script.js
@@ -0,0 +1,11 @@
+var audio = document.createElement('audio');
+audio.setAttribute('sound.mp3');
+
+audio.loop = true;
+audio.play();
+
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'assets/sound.mp3');
+
+audio.loop = true;
+audio.play();
